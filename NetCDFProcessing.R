765d0f06de20df53d11834f64a6de42df520f09c
cahhansen <carlyahansen@gmail.com>
2017-06-28 16:45:28 -0500

    get value for example reach

--- NetCDFProcessing.R
+++ NetCDFProcessing.R
@@ -5,17 +5,19 @@ setwd(directory)
 yearfilelist=shell('dir /b', intern=TRUE)
 
 start.time = Sys.time()
+#Cycle through the years
 for (m in yearfilelist){
   
  yearfolder=paste0(directory,"/",m) 
  ncdffilelist=shell('dir /b', intern=TRUE)
-
+#Cycle through the hourly time steps
  for (n in ncdffilelist){
    ncdffile=paste0(yearfolder,"/",n)
    nwmfile=nc_open(ncdffile,readunlim=FALSE)
    #Get streamflow (in cms)
    nwmdata=ncvar_get(nwmfile,
                      varid="streamflow",
+                     start=c(250,1), #Start value for dimensions (first value is stream reach identifier)
                      count = c(1,-1)) #Number of values to get in each dimension
    nc_close(nwmfile)
  }
@@ -32,6 +34,4 @@ retrodata=nc_open(i)
 streamflow=ncvar_get(retrodata,start=c(1,1), count = c(1,1))
 }
 
-
-...Relevent codes...
 
