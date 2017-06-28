08d7a2bd6e3bb689986b2fab0bcce5fb6229fbde
cahhansen <carlyahansen@gmail.com>
2017-06-28 15:59:02 -0500

    Loop through list of NetCDF data

--- NetCDFProcessing.R
+++ NetCDFProcessing.R
@@ -1,4 +1,30 @@
-#Cycle through files and load
-ncdffile=('Desktop/SummerInstitute/retro_nwm_v10_streamflow/1993/199301010000_streamflow.nc')
-retrodata=nc_open(ncdffile)
-print(retrodata)
+library(ncdf4)
+directory='C:/Users/carly/Desktop/SummerInstitute/retro_nwm_v10_streamflow'
+setwd(directory)
+yearfilelist=shell('dir /b', intern=TRUE)
+
+start.time = Sys.time()
+for (m in yearfilelist){
+  
+ yearfolder=paste0(directory,"/",m) 
+ setwd(yearfolder)
+ ncdffilelist=shell('dir /b', intern=TRUE)
+ for (n in ncdffilelist){
+   ncdffile=paste0(ncdffilelist,"/",n)
+ }
+}
+
+end.time = Sys.time()
+time.taken = end.time - start.time
+time.taken
+
+
+
+for (i in netcdffiles){
+retrodata=nc_open(i)
+streamflow=ncvar_get(retrodata,start=c(1,1), count = c(1,1))
+}
+
+
+...Relevent codes...
+
