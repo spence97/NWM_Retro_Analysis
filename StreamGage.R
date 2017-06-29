4e4245ea89751e2552b231b1699f33c93fc3f6c1
Spencer McDonald <spencermcdonald@gmail.com>
2017-06-29 10:23:09 -0600

    Fixes the "no data" issue in the dataframe formation loop

--- StreamGage.R
+++ StreamGage.R
@@ -20,8 +20,8 @@ library(rgeos)
 stream_subset = NHD[State_Select,]
 
 # Subset the NHD stream network in the state by a user defined stream order then overlay the streams on the state map
-stream_subset1 = stream_subset[stream_subset$order_ > 3,]
-lines(stream_subset1)
+#tream_subset1 = stream_subset[stream_subset$order_ > 3,]
+lines(stream_subset)
 
 # Subset the above subset to select only the streams in NHD that have a corresponding USGS gage
 stream_subset2 = stream_subset1[(stream_subset1$gages != " "),]
@@ -36,20 +36,26 @@ levels(stream_subset2$gages)
 # Create a list of the stream gage IDs that are assimilated to the NHD stream network
 library(dataRetrieval)
 Gage_list = as.character(stream_subset2$gages)
-Gage_list1 = Gage_list[1:4] #used to speed up debugging process (only looks at first 4 gageIDs in list)
+Gage_list1 = Gage_list[137:145] #used to speed up debugging process (only looks at first 4 gageIDs in list)
 
 #create a new data frame that will hold the streamflow time series for all gages at a specified time period
-Observations = data.frame(Date=seq.Date(as.Date("2001-11-01"),as.Date("2001-12-01"),by=1))
+Observations = data.frame(Date=seq(as.Date("1993-01-01"),as.Date("2016-10-31"),by=1))
+
 
 # Loops through the list of gageIDs and appends a new column containing the daily streamflow values for each gageID (the column name is the gageID).
-for (i in Gage_list1){
-  dailyq = readNWISdata(sites=i, service="dv", parameterCd="00060", startDate="2001-11-01", endDate="2001-12-01")
-  df = data.frame(Gage_ID = dailyq$X_00060_00003)
-  names(df)[ncol(df)] = paste0(i)
-  Observations = cbind(Observations,df)
-}
-
+library(dplyr)
+for (i in Gage_list){
+  dailyq = readNWISdata(sites=i, service="dv", parameterCd="00060", startDate="1993-01-01", endDate="2016-10-31")
+  if (nrow(dailyq) > 0){ 
+    df = data.frame(Date = as.Date(dailyq$dateTime), Gage_ID = dailyq$X_00060_00003)
+    names(df)[ncol(df)] = paste0(i)
+    Observations = left_join(Observations, df, by="Date")
+  }
   
+} 
+
+
+
 
 
 
