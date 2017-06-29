e8fad06ce9346695c8c3d7b9f77225cecddfc6fd
cahhansen <carlyahansen@gmail.com>
2017-06-29 14:08:46 -0500

    Calculate daily values for single reach
    Runs on a yearly basis

--- NetCDFProcessing.R
+++ NetCDFProcessing.R
@@ -1,37 +1,53 @@
 library(ncdf4)
-#Set working directory to folder with NetCDF data (stored in different folders for each year)
+#Set working directory to folder with a year of NetCDF data
 directory=getwd()
-setwd(directory)
-yearfilelist=shell('dir /b', intern=TRUE)
+#Set reach (From list of subsetted reaches/gages)
+comid=22548483
 
-start.time = Sys.time()
-#Cycle through the years
-for (m in yearfilelist){
-  
- yearfolder=paste0(directory,"/",m) 
- ncdffilelist=shell('dir /b', intern=TRUE)
-#Cycle through the hourly time steps
- for (n in ncdffilelist){
-   ncdffile=paste0(yearfolder,"/",n)
-   nwmfile=nc_open(ncdffile,readunlim=FALSE)
-   #Get streamflow (in cms)
-   nwmdata=ncvar_get(nwmfile,
-                     varid="streamflow",
-                     start=c(250,1), #Start value for dimensions (first value is stream reach identifier)
-                     count = c(1,-1)) #Number of values to get in each dimension
-   nc_close(nwmfile)
- }
+hourList=sprintf("%02d",seq(0,23,by=1))
+year=substr(directory,nchar(directory)-4+1, nchar(directory))
+is.leapyear=function(year){
+  return(((year %% 4 == 0) & (year %% 100 != 0)) | (year %% 400 == 0))
 }
+dates=read.csv('C:/Users/carly/Desktop/SummerInstitute/DateList.csv')
+if (is.leapyear(as.numeric(year))){
+  dateList=sprintf("%04d",dates$LeapYear)
+}else{
+  dateList=sprintf("%04d",dates$NonLeapYear) 
+  dateList=dateList[(dateList!="  NA")]
+}
+
+#Cycle through the hourly time steps and convert to daily
+ncdfFileList=shell('dir /b', intern=TRUE)
+start.time = Sys.time()
+dailyQDF=data.frame(Date=dateList)
+for (y in dateList){
+  hourlyQDF=data.frame(Hour=hourList)
+  for (x in hourList){
+    ncdfFileName=paste0(year,y,x,"00_streamflow.nc")
+    ncdfFile=paste0(directory,"/",ncdfFileName)
+    nwmFile=nc_open(ncdfFile,readunlim=FALSE)
+    #Get index of COMID from netcdf (dim=1 is the stream reach)
+    featureIDList=nwmFile$dim$feature_id$vals
+    featureIndex=match(comid,featureIDList)
+    
+    #Get streamflow (in cms)
+    nwmData=ncvar_get(nwmFile,
+                   varid="streamflow",
+                   start=c(featureIndex,1), #Start value for dimensions (first value is stream reach index)
+                   count = c(1,-1)) #Number of values to get in each dimension
+    nc_close(nwmFile)
+    hourlyQDF[(hourlyQDF$Hour==x),"Reach"]=nwmData
+  }
+  meanDailyQ=mean(hourlyQDF$Reach)
+  dailyQDF[(dailyQDF$Date==y),"Reach"]=meanDailyQ
+}
+names(dailyQDF)[ncol(dailyQDF)]=reach
 
 end.time = Sys.time()
 time.taken = end.time - start.time
 time.taken
 
+save(dailyQDF,filename=paste0("C:/Users/carly/Desktop/SummerInstitute/",year,".csv"))
 
-
-for (i in netcdffiles){
-retrodata=nc_open(i)
-streamflow=ncvar_get(retrodata,start=c(1,1), count = c(1,1))
-}
-
-
+save(testdf$test,file = "C:/Users/carly/Desktop/SummerInstitute/COMID.csv")
